f4ec1e0c53a385aabb2114d44cd3c52aacaf4fc4
Diego Doná <diedona@gmail.com>
2021-09-22 20:15:50 -0300

    add another product

--- DDona.DevOpsTesting.WebAPI/Controllers/ProductController.cs
+++ DDona.DevOpsTesting.WebAPI/Controllers/ProductController.cs
@@ -14,7 +14,7 @@ namespace DDona.DevOpsTesting.WebAPI.Controllers
         [HttpGet]
         public ActionResult<string[]> Get()
         {
-            return Ok(new string[] { "Laptop", "Web Cam", "Head Set", "Curso da Debrão de Drops" });
+            return Ok(new string[] { "Laptop", "Web Cam", "Head Set", "Curso da Debrão de Drops", "Multi Stage YAML" });
         }
     }
 }
