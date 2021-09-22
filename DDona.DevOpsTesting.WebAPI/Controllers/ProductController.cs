90473244a0883e723029e2ca090079f1f07177cd
Diego Doná <diedona@gmail.com>
2021-09-22 11:29:08 -0300

    add product controller

---
+++ DDona.DevOpsTesting.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDona.DevOpsTesting.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return Ok(new string[] { "Laptop", "Web Cam", "Head Set" });
+        }
+    }
+}
